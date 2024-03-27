79079ffe14312b4b15df4b56c8d9840efa003ed2
mbar3ck7 <ms.mbareck@gmail.com>
2024-03-27 03:31:47 +0000

    web - http methods

--- web/main.py
+++ web/main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -15,6 +15,19 @@ def user(username):
 @app.route('/post/<int:post_id>')
 def post(post_id):
     return '<h1>Post number %s.</h1>' % post_id 
+
+# HTTP methods
+
+@app.route('/http')
+def http():
+    return 'This is a %s method' % request.method
+
+@app.route('/http2', methods=['GET','POST'])
+def http2():
+    if request.method == 'POST':
+        return 'This is a POST method'
+    else:
+        return 'This is probably a GET method'
 
 if __name__ == '__main__':
     app.run(debug=True)
