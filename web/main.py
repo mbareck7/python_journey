b00cd224352681eefd19604010d92eeb1575d28e
mbar3ck7 <ms.mbareck@gmail.com>
2024-03-27 03:14:48 +0000

    web - routing & variables

--- web/main.py
+++ web/main.py
@@ -6,5 +6,15 @@ app = Flask(__name__)
 def index():
     return 'This is a web page :)'
 
+# Routing & variables
+
+@app.route('/user/<username>')
+def user(username):
+    return '<h1>Hi %s, how are you doing ?</h1>' % username
+
+@app.route('/post/<int:post_id>')
+def post(post_id):
+    return '<h1>Post number %s.</h1>' % post_id 
+
 if __name__ == '__main__':
     app.run(debug=True)
